d1f9bc78e818279aaa65782ebdcd95eba6360267
jiangxiny <43875900+jiangxiny@users.noreply.github.com>
2020-10-12 09:05:18 +0800

    小程序表单登录注册

---
+++ biaodan/pages/login/login.js
@@ -0,0 +1,26 @@
+ // pages/login/login.js 
+ Page({   
+   data: {     
+     disabled: true,     
+     btnstate: "default",     
+     account:"",     
+     password:""   
+    },   
+    accountInput: function (e){
+     var content = e.detail.value;
+     console.log(content);
+     if(content!=""){
+       this.setData({disabled:false,btnstate:"primary",account:content});
+     }else{
+      this.setData({disabled:true,btnstate:"default"});
+     }
+   },
+   pwdBlur: function (e) {
+     var password = e.detail.value;
+     if(password!=""){
+       this.setData({password:password});
+     }
+   }
+ })
+
+ 
